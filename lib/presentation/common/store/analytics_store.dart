cc93898e26ace7dbffa693961a55f74b287be25c
Elvinas <elviz.nejuz@gmail.com>
2024-05-10 03:40:05 +0300

    fix lint

--- lib/presentation/common/store/analytics_store.dart
+++ lib/presentation/common/store/analytics_store.dart
@@ -4,14 +4,12 @@ import 'package:news_app/domain/use-cases/log_event.dart';
 
 part 'analytics_store.g.dart';
 
+// ignore: library_private_types_in_public_api
 class AnalyticsStore = _AnalyticsStore with _$AnalyticsStore;
 
 abstract class _AnalyticsStore with Store {
   _AnalyticsStore(this._logEvent);
   final LogEvent _logEvent;
-
-  @observable
-  bool isEventLogged = false;
 
   @action
   Future<void> logEvent(
@@ -22,6 +20,5 @@ abstract class _AnalyticsStore with Store {
       name: eventName,
       parameters: parameters ?? {},
     );
-    isEventLogged = true;
   }
 }
--- lib/presentation/common/store/analytics_store.g.dart
+++ lib/presentation/common/store/analytics_store.g.dart
@@ -9,22 +9,6 @@ part of 'analytics_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$AnalyticsStore on _AnalyticsStore, Store {
-  late final _$isEventLoggedAtom =
-      Atom(name: '_AnalyticsStore.isEventLogged', context: context);
-
-  @override
-  bool get isEventLogged {
-    _$isEventLoggedAtom.reportRead();
-    return super.isEventLogged;
-  }
-
-  @override
-  set isEventLogged(bool value) {
-    _$isEventLoggedAtom.reportWrite(value, super.isEventLogged, () {
-      super.isEventLogged = value;
-    });
-  }
-
   late final _$logEventAsyncAction =
       AsyncAction('_AnalyticsStore.logEvent', context: context);
 
@@ -37,7 +21,7 @@ mixin _$AnalyticsStore on _AnalyticsStore, Store {
   @override
   String toString() {
     return '''
-isEventLogged: ${isEventLogged}
+
     ''';
   }
 }
